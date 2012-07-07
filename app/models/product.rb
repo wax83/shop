482ecd779379c2fcbc475d132d5305dff6fc42eb
Zeppelin <gabor.babicz@shellshaper.com>
2012-07-07 15:52:45 +0200

    Added annotation for Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  created_at  :datetime         not null
+#  description :text
+#  id          :integer          not null, primary key
+#  name        :string(255)
+#  price       :float
+#  published   :boolean
+#  updated_at  :datetime         not null
+#
+
 class Product < ActiveRecord::Base
   attr_accessible :description, :name, :price, :published
 end
--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  created_at  :datetime         not null
+#  description :text
+#  id          :integer          not null, primary key
+#  name        :string(255)
+#  price       :float
+#  published   :boolean
+#  updated_at  :datetime         not null
+#
+
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
